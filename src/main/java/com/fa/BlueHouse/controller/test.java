d9f0a2aad34f0235ac984e74aeba918db0ca6064
dung <kinnguyen271097@gmail.com>
2024-07-25 15:40:00 +0700

    Start here

---
+++ src/main/java/com/fa/BlueHouse/controller/test.java
@@ -0,0 +1,12 @@
+package com.fa.BlueHouse.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class test {
+	@GetMapping("/")
+	public String testHome() {
+		return "test";
+	}
+}
---
+++ src/test/java/com/fa/BlueHouse/BlueHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fa.BlueHouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
