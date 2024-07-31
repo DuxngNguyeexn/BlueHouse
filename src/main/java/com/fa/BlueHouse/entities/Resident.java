85aab05aa952e5b6058e1c7e9125baf1ee088c50
manh <tienmanh2002.it@gmail.com>
2024-07-31 14:39:47 +0700

    manh commit

--- src/main/java/com/fa/BlueHouse/entities/Resident.java
+++ src/main/java/com/fa/BlueHouse/entities/Resident.java
@@ -1,12 +1,20 @@
 package com.fa.BlueHouse.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Resident {
+	@Id
+	private String idResident;
 	@ManyToOne
-	@JoinColumn(name = "ID_Resident")
+	@JoinColumn(name = "ID_APARTMENT")
 	private Apartment apartment;
+
+	public Resident() {
+		super();
+	}
+	
 }
