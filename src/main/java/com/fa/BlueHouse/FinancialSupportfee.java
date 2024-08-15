af18fb2f820f9b7362401a01f8887149fcae9b88
manh <tienmanh2002.it@gmail.com>
2024-08-15 15:57:31 +0700

    tienmah

---
+++ src/main/java/com/fa/BlueHouse/FinancialSupportfee.java
@@ -0,0 +1,76 @@
+package com.fa.BlueHouse;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class FinancialSupportfee {
+
+	@Id
+	private String billCode;
+	
+	private String nameFeeType;
+	
+	private String sponsorName;
+	
+	private LocalDate date;
+	
+	private float price;
+
+	public FinancialSupportfee() {
+		super();
+	}
+
+	public FinancialSupportfee(String billCode, String nameFeeType, String sponsorName, LocalDate date, float price) {
+		super();
+		this.billCode = billCode;
+		this.nameFeeType = nameFeeType;
+		this.sponsorName = sponsorName;
+		this.date = date;
+		this.price = price;
+	}
+
+	public String getBillCode() {
+		return billCode;
+	}
+
+	public void setBillCode(String billCode) {
+		this.billCode = billCode;
+	}
+
+	public String getNameFeeType() {
+		return nameFeeType;
+	}
+
+	public void setNameFeeType(String nameFeeType) {
+		this.nameFeeType = nameFeeType;
+	}
+
+	public String getSponsorName() {
+		return sponsorName;
+	}
+
+	public void setSponsorName(String sponsorName) {
+		this.sponsorName = sponsorName;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+	
+}
