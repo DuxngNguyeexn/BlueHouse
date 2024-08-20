0a44763090ac8400506838a49c46b5cdb02cd36a
Huy Do <dohuy241020@gmail.com>
2024-08-20 13:11:01 +0700

    update noti

--- src/main/java/com/fa/BlueHouse/controller/NotiController.java
+++ src/main/java/com/fa/BlueHouse/controller/NotiController.java
@@ -1,5 +1,10 @@
 package com.fa.BlueHouse.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.Principal;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -14,11 +19,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.fa.BlueHouse.authen.model.AccountDTO;
 import com.fa.BlueHouse.entities.Employee;
 import com.fa.BlueHouse.entities.Notification;
-import com.fa.BlueHouse.entities.Receiver;
 import com.fa.BlueHouse.entities.Resident;
 import com.fa.BlueHouse.services.EmployeeService;
 import com.fa.BlueHouse.services.NotiService;
@@ -27,6 +33,8 @@ import com.fa.BlueHouse.services.ResidentService;
 @Controller
 @RequestMapping(path = "notification")
 public class NotiController {
+	private final String uploadDirRequest = "E:\\imgNoti";
+	private Notification notificationSend;
 
 	@Autowired
 	private NotiService notiService;
@@ -37,14 +45,31 @@ public class NotiController {
 	@Autowired
 	private ResidentService rService;
 
+	@GetMapping("viewDetail")
+	public String viewDetail(@RequestParam(name = "idNoti", defaultValue = "") String idNoti, Model model) {
+
+		Notification listNoti = notiService.findNotiByID("R0132024-08-2013:08:14.975624300");
+
+		model.addAttribute("post", listNoti);
+
+		return "Notifications/viewNoti";
+	}
+
 	@GetMapping("listSeen")
-	public String listSeen() {
+	public String listSeen(Principal principal, Model model) {
+		AccountDTO auth = (AccountDTO) ((Authentication) principal).getPrincipal();
+
+		List<Notification> listNoti = notiService.findByIDSeen(auth.getId());
+
+		for (Notification e : listNoti) {
+			System.err.println(e.getNotificationCode());
+		}
+
 		return "Notifications/listSeenNoti";
 	}
 
 	@GetMapping("listSend")
 	public String listSend(Principal principal, Model model) {
-
 		AccountDTO auth = (AccountDTO) ((Authentication) principal).getPrincipal();
 
 		List<Notification> listNoti = notiService.findByIDSend(auth.getId());
@@ -55,8 +80,6 @@ public class NotiController {
 
 		return "Notifications/listSendNoti";
 	}
-
-	private Notification notificationSend;
 
 	/**
 	 * Điều hướng tới trang tạo noti
@@ -75,10 +98,24 @@ public class NotiController {
 	 * @return notificationSend
 	 */
 	@PostMapping("save")
-	public String save(Model model, @ModelAttribute("noti") Notification noti, Principal principal) {
+	public String save(Model model, @ModelAttribute("noti") Notification noti, Principal principal,
+			@RequestParam("imgNotification") MultipartFile file, RedirectAttributes redirectAttributes) {
+
+		if (file != null) {
+			try {
+				String fileName = file.getOriginalFilename();
+				Path path = Paths.get(uploadDirRequest + File.separator + fileName);
+				Files.write(path, file.getBytes());
+				noti.setAttachment(fileName);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
 		AccountDTO thongTin = (AccountDTO) ((Authentication) principal).getPrincipal();
 		noti.setDate(LocalDate.now());
 		noti.setTime(LocalTime.now());
+
 		notificationSend = noti;
 
 		String id = thongTin.getId() + notificationSend.getDate() + notificationSend.getTime();
@@ -108,62 +145,16 @@ public class NotiController {
 	public String saveReceiver(Principal principal,
 			@RequestParam(name = "ListValue", defaultValue = "") String listRecei,
 			@RequestParam(name = "choose", defaultValue = "0") String choose) {
-
 		AccountDTO thongTin = (AccountDTO) ((Authentication) principal).getPrincipal();
-
-		String[] listReceiver = listRecei.split(",");
 
 		Employee senderEmp = eService.findById(thongTin.getId());
 		Resident senderResi = rService.findById(thongTin.getId());
 
-		if (choose.equalsIgnoreCase("All")) {
-			notiService.saveNoti(notificationSend);
-			saveAll(notificationSend, senderEmp, senderResi, eService.allEmployee(), rService.findallResident());
-		}
-		if (choose.equalsIgnoreCase("AllEmployee")) {
-			notiService.saveNoti(notificationSend);
-			saveAllEmp(notificationSend, senderEmp, senderResi, eService.allEmployee());
-		}
-		if (choose.equalsIgnoreCase("AllResident")) {
-			notiService.saveNoti(notificationSend);
-			saveAllResi(notificationSend, senderEmp, senderResi, rService.findallResident());
-		}
-		if (choose.equalsIgnoreCase("Choosen")) {
-			notiService.saveNoti(notificationSend);
-			saveChoosen(notificationSend, senderEmp, senderResi, listReceiver);
-		}
+		String[] listReceiver = listRecei.split(",");
+
+		notiService.saveNotificationAndReceiver(choose, notificationSend, senderEmp, senderResi, listReceiver);
 
 		return "redirect:add";
-	}
-
-	private void saveAll(Notification noti, Employee senderEmp, Resident senderResi, List<Employee> listEmp,
-			List<Resident> listResi) {
-		for (Employee e : listEmp) {
-			notiService.saveRecei(new Receiver(noti, senderEmp, senderResi, e, null));
-		}
-		for (Resident e : listResi) {
-			notiService.saveRecei(new Receiver(noti, senderEmp, senderResi, null, e));
-		}
-	}
-
-	private void saveAllEmp(Notification noti, Employee senderEmp, Resident senderResi, List<Employee> listEmp) {
-		for (Employee e : listEmp) {
-			notiService.saveRecei(new Receiver(noti, senderEmp, senderResi, e, null));
-		}
-	}
-
-	private void saveAllResi(Notification noti, Employee senderEmp, Resident senderResi, List<Resident> listResi) {
-		for (Resident e : listResi) {
-			notiService.saveRecei(new Receiver(noti, senderEmp, senderResi, null, e));
-		}
-	}
-
-	private void saveChoosen(Notification noti, Employee senderEmp, Resident senderResi, String[] listID) {
-		for (String e : listID) {
-			Employee emp = eService.findById(e);
-			Resident resi = rService.findById(e);
-			notiService.saveRecei(new Receiver(noti, senderEmp, senderResi, emp, resi));
-		}
 	}
 
 }
--- src/main/java/com/fa/BlueHouse/repositories/ReceiverRepositories.java
+++ src/main/java/com/fa/BlueHouse/repositories/ReceiverRepositories.java
@@ -12,4 +12,7 @@ import com.fa.BlueHouse.entities.Receiver;
 public interface ReceiverRepositories extends JpaRepository<Receiver, String> {
 	@Query("SELECT DISTINCT recei.notification FROM Receiver recei WHERE recei.senderEmp.employeeID = :idEmp OR recei.senderResi.idResident = :idEmp")
 	List<Notification> findByIDSend(@Param("idEmp") String idEmp);
+	
+	@Query("SELECT DISTINCT recei.notification FROM Receiver recei WHERE recei.receiverEmp.employeeID = :idEmp OR recei.receiverResi.idResident = :idEmp")
+	List<Notification> findByIDSeen(@Param("idEmp") String idEmp);
 }
