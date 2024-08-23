57e62a4f538022c64abd6a0730a6cc23c6ef6c08
Duxng <kinnguyen271097@gmail.com>
2024-08-23 14:17:42 +0700

    Dung

--- src/main/java/com/fa/BlueHouse/controller/CustomErrorController.java
+++ src/main/java/com/fa/BlueHouse/controller/CustomErrorController.java
@@ -3,12 +3,12 @@ package com.fa.BlueHouse.controller;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-@Controller
+//@Controller
 public class CustomErrorController implements ErrorController {
 
-	@RequestMapping("/error")
-    public String handleError() {
-        return "Error/404"; 
-    }
+//	@RequestMapping("/error")
+//    public String handleError() {
+//        return "Error/404"; 
+//    }
 
 }
